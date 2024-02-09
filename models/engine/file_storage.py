fd070975677af6c3f38a0fe22610b34a6dc9f314
Voxold <bilalajaha@gmail.com>
2024-02-09 04:30:40 -0800

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 import json
+import models.base_model import BaseModel
+import models.user import User
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
+    __file_path = "file.json"
+    __objects = {"BaseModel": BaseModel, "User": User}
 
     def all(self):
         """Returns the dictionary __objects"""
