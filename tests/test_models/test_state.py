78a052eecc09598313b6f30643b89b62e27e5d9b
Voxold <bilalajaha@gmail.com>
2024-02-10 15:00:03 -0800

    test_state

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import unittest
+from models.state import state
+
+class TestState(unittest.TestCase):
+    """State Test"""
+
+    def test_atr(self):
+        """State Attribytes Test"""
+
+        result = State()
+        self.assertEqual(result.name,'')
+
+if __name__ == "__main":
+    unittest.main()
