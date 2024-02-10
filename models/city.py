8f12a14e54968ef5bad265df3691836d63bf05d3
Voxold <bilalajaha@gmail.com>
2024-02-10 03:57:44 -0800

    amenity

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+form models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class inherits from BaseModel."""
+
+    # Public class attribute
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+
+class state(BaseModel):
+    """State class inherits from BaseModel."""
+    
+    name = ""
