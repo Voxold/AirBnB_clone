f99b228bf2f62659b50a5d9fb139d3e65a277f6f
Voxold <bilalajaha@gmail.com>
2024-02-10 14:30:05 -0800

    test_amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python3
+
+
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """Unittest for Amenity file"""
+
+    def test_attributes(self):
+        """Test for Amenity attributes"""
+
+        result = Amenity()
+        self.assertEqual(result.name, '')
+
+if __name__ == "__main__":
+    unittest.main()
