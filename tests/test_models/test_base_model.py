568501e07b697ac44954b8e70eaed684f0c85754
Voxold <bilalajaha@gmail.com>
2024-02-18 09:26:35 -0800

    upd

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,59 +1,41 @@
 #!/usr/bin/python3
-"""Unittest for BaseModel"""
-
-
 import unittest
-from datetime import datetime
 from models.base_model import BaseModel
-
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
-    """unittest_BaseModel"""
 
-    def test_types(self):
-        """test for types"""
+    def test_output(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
 
-        obj = BaseModel()
-        obj.name = "My First Model"
-        obj.my_number = 89
-        self.assertEqual(obj.name, "My First Model")
-        self.assertEqual(obj.my_number, 89)
-        self.assertIsInstance(obj.id, str)
-        self.assertIsInstance(obj.updated_at, datetime)
-        self.assertIsInstance(obj.created_at, datetime)
-        self.assertIsInstance(obj.name, str)
-        self.assertIsInstance(obj.__class__.__name__, str)
-        self.assertEqual(obj.__class__.__name__, 'BaseModel')
-        self.assertIsInstance(obj.my_number, int)
-        pr = f"[{obj.__class__.__name__}] ({obj.id}) {obj.__dict__}"
-        self.assertEqual(str(obj), pr)
+        expected_output = (
+            f'[BaseModel] ({my_model.id}) '
+            f"{{'my_number': 89, 'name': 'My First Model', "
+            f"'updated_at': {repr(my_model.updated_at)}, "
+            f"'id': '{my_model.id}', "
+            f"'created_at': {repr(my_model.created_at)}}}"
+        )
 
-        obj_json = obj.to_dict()
+        with self.assertLogs() as logs:
+            print(my_model)
 
-        self.assertEqual(obj_json['name'], "My First Model")
-        self.assertEqual(obj_json['my_number'], 89)
-        self.assertIsInstance(obj_json['id'], str)
-        self.assertIsInstance(obj_json['updated_at'], str)
-        self.assertIsInstance(obj_json['created_at'], str)
-        self.assertIsInstance(obj_json['name'], str)
-        self.assertIsInstance(obj_json['__class__'], str)
-        self.assertEqual(obj_json['__class__'], 'BaseModel')
-        self.assertIsInstance(obj_json['my_number'], int)
-        self.assertIsInstance(obj_json, dict)
-        self.assertEqual
-        (obj_json['updated_at'], datetime.isoformat(obj.updated_at))
-        self.assertEqual
-        (obj_json['created_at'], datetime.isoformat(obj.created_at))
+        self.assertEqual(logs.output, [expected_output])
 
-        update_time = obj.updated_at
+        my_model.save()
+        expected_updated_output = (
+            f'[BaseModel] ({my_model.id}) '
+            f"{{'my_number': 89, 'name': 'My First Model', "
+            f"'updated_at': {repr(my_model.updated_at)}, "
+            f"'id': '{my_model.id}', "
+            f"'created_at': {repr(my_model.created_at)}}}"
+        )
 
-        obj.save()
+        with self.assertLogs() as logs:
+            print(my_model)
 
-        update_time2 = obj.updated_at
-        self.assertNotEqual(update_time, update_time2)
+        self.assertEqual(logs.output, [expected_updated_output])
 
-        self.assertNotEqual(obj.updated_at, obj.created_at)
-
-        obj2 = BaseModel()
-        self.assertNotEqual(obj.id, obj2.id)
-        self.assertNotEqual(obj, obj2)
+if __name__ == '__main__':
+    unittest.main()
