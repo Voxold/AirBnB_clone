d3f9e058a856f6a06c992133073676ae8f1892ce
Voxold <bilalajaha@gmail.com>
2024-02-18 09:05:23 -0800

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,57 +1,37 @@
 #!/usr/bin/python3
+"""initialization, serialization and deserialization"""
 
+import datetime
 import uuid
-from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-    """
-    BaseModel class for common attributes/methods for other classes.
-    """
+    """class BaseModel that defines all common \
+    attributes/methods for other classes"""
 
-    def __init__(self, *args, **kwargs):
-        """
-        Initiates the BaseModel with a unique ID \
-        and timestamps for creation and updates.
-        """
+    def __init__(self) -> None:
+        """Public instance attributes"""
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        self.__dict__[key] = datetime.fromisoformat(value)
-                    else:
-                        self.__dict__[key] = value
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    def __str__(self):
-        """
-        Returns a string representation of the BaseModel instance.
-        """
-
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __str__(self) -> str:
+        """should print..."""
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        """
-        Updates the 'updated_at' attribute with the current datetime.
-        """
+        """updates the public instance attribute """
 
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """
-        Converts the BaseModel instance to a dictionary.
-        """
+        """returns a dictionary containing all keys/values"""
 
-        model_dict = self.__dict__.copy()
-        model_dict['__class__'] = self.__class__.__name__
-        model_dict['created_at'] = self.created_at.isoformat()
-        model_dict['updated_at'] = self.updated_at.isoformat()
-
-        return model_dict
+        dictionary = dict(self.__dict__)
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = \
+            datetime.isoformat(dictionary.get('created_at'))
+        dictionary['updated_at'] = \
+            datetime.isoformat(dictionary.get('updated_at'))
+        return dictionary
